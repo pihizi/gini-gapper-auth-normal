18b9ae936457841ca92a5a6882a7d8fcae967668
pihizi <pihizi@msn.com>
2015-03-25 11:24:18 +0800

    可以找gapper-server要app的URL，不需要自己再单独配置app.home dignose的检测可以去掉

--- class/Gini/Module/GapperAuthTust.php
+++ class/Gini/Module/GapperAuthTust.php
@@ -10,10 +10,6 @@ class GapperAuthTust
 
     public static function diagnose()
     {
-        $home = \Gini\Config::get('app.home');
-        if (!$home) {
-            return ['请配置app.home'];
-        }
         $secret = \Gini\Config::get('app.tust_secret');
         if (!$secret) {
             return ['需要提供app.tust_secret'];
